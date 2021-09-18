485693b88c69fe392409be7aebd16de8f6621f75
Eingherjar <47706950+Eingherjar@users.noreply.github.com>
2021-09-17 23:36:51 -0500

    Eliminar consoles log del componente shared

--- proyecto/src/app/shared/shared.component.ts
+++ proyecto/src/app/shared/shared.component.ts
@@ -20,30 +20,24 @@ export class SharedComponent implements OnInit {
   login_events(e) {
     switch (e.event) {
       case 'login':
-        console.log("entra en el evento de login en shared")
         this.servicio.login(e.data).subscribe((data: any) => {
-          console.log("datos que trae el servicio de login events al componente shared", data);
           if(data.estado === 'success'){
             this.config_login={
               event:"login",
               id_usuario: data.usuario.id_usuario,
               rol:data.usuario.rol
             }
-
+            
             this.notifier.notify("success", data.mensaje);
 
-            
           }else if (data.estado === 'error'){
             this.notifier.notify("error", "ha ocurrido un error");
-            console.log("error",data);
-
           }
 
         })
       break;
 
       case 'validar_usuario':
-        console.log("entro en el evento de validar_usuario en shared");
         this.servicio.validar_usuario(e.data).subscribe((data:any)=>{
 
           if(data.estado === 'success'){
@@ -59,7 +53,6 @@ export class SharedComponent implements OnInit {
             
           }else if (data.estado === 'error'){
             this.notifier.notify("error", "ha ocurrido un error");
-            console.log("error",data);
 
           }
         })
@@ -94,7 +87,6 @@ export class SharedComponent implements OnInit {
           this.notifier.notify("success",data.error.mensaje);
         }else if (data.estado === 'error' && data.error.id_alerta !=2){
           this.notifier.notify("error","error al crear el usuario");
-          console.log("datos del error de crear el usuario",data);
         }
       })
       break;
